4de2a1443c686fbdfd228e6880c9eaf9c707fa7c
yuki28git <yoshi.1012.programing@gmail.com>
2026-06-27 15:51:26 +0900

    自動化とSEの実装

--- Assets/Script/VolumeControllerScript.cs
+++ Assets/Script/VolumeControllerScript.cs
@@ -1,19 +1,82 @@
+/*
 using UnityEngine;
 using UnityEngine.UI;
 
 public class VolumeController : MonoBehaviour
 {
-    public Slider volumeSlider;
-
-    // 最初の画面表示時の処理
-    void Start()
+    public enum VolumeType
     {
-        volumeSlider.value = AudioListener.volume;
+        BGM,
+        ClickSE
     }
 
-    // ボリュームを変更するメソッド
-    public void SetVolume(float volume)
+    [SerializeField] private Slider volumeSlider;
+    [SerializeField] private VolumeType volumeType;
+
+    void Awake()
     {
-        AudioListener.volume = volume;
+        EnsureDefaultValue();
+        ApplySavedVolumeToSlider();
+    }
+
+    void OnEnable()
+    {
+        ApplySavedVolumeToSlider();
+    }
+
+    void EnsureDefaultValue()
+    {
+        string key = GetKey(volumeType);
+
+        if (!PlayerPrefs.HasKey(key))
+        {
+            PlayerPrefs.SetFloat(key, 1f);
+            PlayerPrefs.Save();
+        }
+    }
+
+    void ApplySavedVolumeToSlider()
+    {
+        if (volumeSlider == null) return;
+
+        volumeSlider.minValue = 0f;
+        volumeSlider.maxValue = 10f;
+        volumeSlider.wholeNumbers = true;
+
+        float savedVolume = GetSavedVolume(volumeType);
+        int sliderValue = Mathf.RoundToInt(savedVolume * 10f);
+        volumeSlider.SetValueWithoutNotify(sliderValue);
+    }
+
+    public void SetVolume(float sliderValue)
+    {
+        int step = Mathf.RoundToInt(sliderValue);
+        float normalizedVolume = step / 10f;
+
+        string key = GetKey(volumeType);
+        PlayerPrefs.SetFloat(key, normalizedVolume);
+        PlayerPrefs.Save();
+
+        Debug.Log($"{volumeType} Volume = {normalizedVolume} (step {step})");
+    }
+
+    string GetKey(VolumeType type)
+    {
+        switch (type)
+        {
+            case VolumeType.BGM:
+                return "BGMVolume";
+            case VolumeType.ClickSE:
+                return "ClickSEVolume";
+            default:
+                return "ClickSEVolume";
+        }
+    }
+
+    public static float GetSavedVolume(VolumeType type)
+    {
+        string key = type == VolumeType.BGM ? "BGMVolume" : "ClickSEVolume";
+        return PlayerPrefs.GetFloat(key, 1f);
     }
 }
+*/
