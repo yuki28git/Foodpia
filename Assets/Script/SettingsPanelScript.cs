185e5cbc8d135d4c80e7b39f819eff4cc0fa46d1
yuki28git <yoshi.1012.programing@gmail.com>
2026-08-15 14:37:29 +0900

    設定画面修正

--- Assets/Script/SettingsPanelScript.cs
+++ Assets/Script/SettingsPanelScript.cs
@@ -1,8 +1,17 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class SettingsUI : MonoBehaviour
 {
     public GameObject settingsPanel;
+
+    void Update()
+    {
+        if (Keyboard.current != null && Keyboard.current.escapeKey.wasPressedThisFrame)
+        {
+            OpenSettings();
+        }
+    }
 
     // 設定パネルを開くメソッド
     public void OpenSettings()
